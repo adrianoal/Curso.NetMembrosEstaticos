cf44cc671ac73a518f25042af7450eeb882f58ba
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-08 16:26:49 -0300

    Valor de uma circuferencia, volume e esfera de um raio

---
+++ MembrosEstaticos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circuferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circuferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Valor de PI: {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        }                             
+    }
+}
